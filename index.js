3faa52d85d358734030a0422765ff1ac394b4a9a
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-11-28 20:23:02 +0100

    setting

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hola mundo');
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+// Routing
+
+app.get('/', (req, res) => {
+      res.send('Hola Mundo en Express');
+})
+
+app.listen(port, () => {
+      console.log('Servidor Funcionando');
+});
