587a68075ad44f6fad6ddd3bcb35bb3b7ec23dd7
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-03 21:47:17 +0100

    adding database mongodb user

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import User from "../models/User";
 
 const router = Router();
 
 // Routing Auth and Register
 
-router.post('/auth/register', (req, res) => {
-      console.log(req.body);
+router.post('/auth/register', async (req, res) => {
+      //console.log(req.body);
+      const user = new User(req.body);
+      await user.save();
 });
 
 
