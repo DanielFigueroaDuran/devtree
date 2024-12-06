802ed6dfc4ea7ae02edbc8469e696552f35c5bd5
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2024-12-06 15:58:12 +0100

    Adding the rest of the validation to the User Registry

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -7,7 +7,19 @@ const router = Router();
 // Routing Auth and Register
 
 router.post('/auth/register',
-      body('handle').notEmpty(),
+      body('handle')
+            .notEmpty()
+            .withMessage('El handle no puede ir vacio'),
+      body('name')
+            .notEmpty()
+            .withMessage('El Nombre no puede ir vacio'),
+      body('email')
+            .isEmail()
+            .withMessage('E-mail no valido'),
+      body('password')
+            .isLength({ min: 8 })
+            .withMessage('El Password es muy corto, minimo 8 caracteres'),
+
       createAccount);
 
 
