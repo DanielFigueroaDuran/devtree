67258758bd992c1e49441ac9f529e6aa3002a864
DanielFigueroaDuran <figueroadurandanielelias@gmail.com>
2025-01-05 21:58:57 +0100

    Querying the database and displaying the information

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -134,7 +134,14 @@ export const uploadImage = async (req: Request, res: Response) => {
 export const getUserByHandle = async (req: Request, res: Response) => {
       try {
             const { handle } = req.params;
-            console.log(handle);
+            const user = await User.findOne({ handle }).select('-_id -__v -email -password');
+            //console.log(user);
+            res.json(user);
+            if (!user) {
+                  const error = new Error('El Usuario no existe');
+                  res.status(404).json({ error: error.message });
+                  return;
+            };
       } catch (e) {
             const error = new Error('Hubo un error');
             res.status(500).json({ error: error.message });
